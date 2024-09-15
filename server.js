6f5990c80d26e7cadc7e76b361dfafcd05aa3824
oswandor26 <oswandor26@gmail.com>
2024-09-14 20:20:08 -0600

    update files server with cors

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const multer = require('multer');
 const { BlobServiceClient } = require('@azure/storage-blob');
+const cors = require('cors'); // Importa el paquete cors
 require('dotenv').config();
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
 const sasToken = process.env.AZURE_STORAGE_SAS_TOKEN;
+
+// const accountName = 'almacenamientodearchiv';
+// const sasToken = 'sp=racwdl&st=2024-09-11T19:39:16Z&se=2024-10-01T03:39:16Z&spr=https&sv=2022-11-02&sr=c&sig=IvRoiP%2BKvb7SbK07UD2LypODCEceEolyOIXAYZ7Bg1M%3D';
 
 if (!accountName) throw Error('Azure Storage accountName not found');
 if (!sasToken) throw Error('Azure Storage SAS token not found');
@@ -17,7 +22,16 @@ const blobServiceClient = new BlobServiceClient(`${blobServiceUri}?${sasToken}`,
 
 const upload = multer({ dest: 'uploads/' });
 
+
+// Configura CORS para permitir solicitudes desde http://localhost:8081
+app.use(cors({
+  origin: 'http://localhost:8081'
+})); 
+
+
 app.use(express.static('public'));
+
+
 
 app.post('/upload', upload.single('file'), async (req, res) => {
   const containerName = 'uploadfiles';
